6592e5c0a00bbf23bb41cb04b5488c77ef2e644c
ao <xibas.fork@gmail.com>
2023-08-12 20:15:55 -0400

    Create Documentation.lua

---
+++ Resources/AirKey/Documentation.lua
@@ -0,0 +1,8 @@
+-- 1
+loadstring(game:HttpGet'https://raw.githubusercontent.com/ao-0/miami-nights/main/Resources/AirKey/Manjaro1_0.lua')()('Swoowsh', 'An innovative script', 'https://work.ink/4iC/ll8lemwj')
+print'Whitelisted!'
+-- 2
+local AirKey = loadstring(game:HttpGet'https://raw.githubusercontent.com/ao-0/miami-nights/main/Resources/AirKey/Manjaro1_0.lua')();
+wait(2)
+AirKey('Ulquiorra EX', 'a plum is a banana of its mind', 'https://work.ink/4iC/ll8lemwj')
+print'Whitelisted!'
